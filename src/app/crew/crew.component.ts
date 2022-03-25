51e6a61fab95ce241e73fdf980458055ba59db59
Your Name~ <timrogersj@gmail.com>
2022-03-24 20:48:11 -0500

    Studio part 2 Done

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -10,7 +10,7 @@ export class CrewComponent implements OnInit {
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    {name: "Ellen Ochoa - 1st", firstMission: true}
   ];
 
   constructor() { }
@@ -18,4 +18,24 @@ export class CrewComponent implements OnInit {
   ngOnInit() {
   }
 
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  memberBeingEdited: object = null;
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
+  
 }
